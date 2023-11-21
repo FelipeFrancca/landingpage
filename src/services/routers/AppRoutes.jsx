b3c08339b4aa5f38646846bf8a1c33604d62790e
FelipeFrancca <suporte.felipefranca@gmail.com>
2023-11-21 20:29:23 -0300

    up

--- src/services/routers/AppRoutes.jsx
+++ src/services/routers/AppRoutes.jsx
@@ -6,7 +6,7 @@ import Dashboard from "../../pages/dashboard/Dashboard";
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="" element={<Dashboard />}>
+      <Route path="/landingpage" element={<Dashboard />}>
         <Route path="" element={<Home />} />
       </Route>
       <Route path="*" element={<NotFound />} />
